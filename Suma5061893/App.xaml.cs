b293095b49d1c630c75cfcf5e0281e031e8d8566
Julissa <Julissa@JULS>
2024-08-19 20:51:46 -0600

    Agregar archivos de proyecto.

---
+++ Suma5061893/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Suma5061893
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Suma5061893/Resultado.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Suma5061893
+{
+    [SQLite.Table("Resultado")]
+    public class Resultado
+    {
+        [PrimaryKey, AutoIncrement]
+        [SQLite.Column("id")]
+        public int Id { get; set; }
+
+        [SQLite.Column("numero1")]
+
+        public string? Numero1 { get; set; }
+        [SQLite.Column("numero2")]
+
+        public string? Numero2 { get; set; }
+
+        [SQLite.Column("suma")]
+        public string? Suma { get; set; }
+    }
+}
